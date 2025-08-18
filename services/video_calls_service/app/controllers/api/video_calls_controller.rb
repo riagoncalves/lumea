ef80acf1272d4ef6f2fda09efc72018522eb5aef
riagoncalves <ricardo.goncalves252@gmail.com>
2025-08-19 00:03:51 +0100

    feat: beggining of video call service implementation

---
+++ services/video_calls_service/app/controllers/api/video_calls_controller.rb
@@ -0,0 +1,54 @@
+module Api
+  class VideoCallsController < ApplicationController
+    def create
+      
+    end
+
+    def show
+      render(status: :ok,
+            json: patient_detail,
+            serializer: ::PatientDetailsSerializer,
+            root: :patient_detail,
+            adapter: :json)
+    rescue ActiveRecord::RecordNotFound
+      render(status: :not_found, json: { errors: ['Patient detail not found'] })
+    end
+
+    def update
+      if patient_detail.update(patient_detail_params)
+        render(status: :ok,
+              json: patient_detail,
+              serializer: ::PatientDetailsSerializer,
+              root: :patient_detail,
+              adapter: :json)
+      else
+        render(status: :unprocessable_entity, json: { errors: patient_detail.errors.full_messages })
+      end
+    rescue ActiveRecord::RecordNotFound
+      render(status: :not_found, json: { errors: ['Patient detail not found'] })
+    end
+
+    private
+
+    def video_call
+    end
+
+    def generate_video_token(identity, room_sid)
+      # Setup token
+      token = Twilio::JWT::AccessToken.new(
+        ENV['TWILIO_ACCOUNT_SID'],
+        ENV['TWILIO_API_KEY_SID'],
+        ENV['TWILIO_API_KEY_SECRET'],
+        identity: identity
+      )
+
+      # Attach a VideoGrant
+      grant = Twilio::JWT::AccessToken::VideoGrant.new
+      grant.room = room_sid
+      token.add_grant(grant)
+
+      # Return the signed JWT
+      token.to_jwt
+    end
+  end
+end
