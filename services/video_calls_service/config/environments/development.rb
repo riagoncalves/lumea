c752a87776e8fe1a1b93d81130cc34e19dc792b1
riagoncalves <ricardo.goncalves252@gmail.com>
2025-11-01 18:20:50 +0000

    fix: webhook dev

--- services/video_calls_service/config/environments/development.rb
+++ services/video_calls_service/config/environments/development.rb
@@ -3,7 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts += %w[video_calls]
+  config.hosts += [
+    "video_calls",
+    /.*\.ngrok-free\.app/
+  ]
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
